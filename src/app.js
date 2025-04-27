e3814eda06b8371e59419361b6a3eb8f1224b932
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-04-27 12:47:21 -0300

    Adicionando rota getAlunosByMatricula

--- src/app.js
+++ src/app.js
@@ -14,13 +14,25 @@ const alunos = [
     { matricula: 122223, nome: "Daniel", curso: "Ciencia da Computação" }
 ]
 
+function getAlunosByMatricula(matricula) {
+    return alunos.filter(aluno => aluno.matricula == matricula)
+}
+
 app.get('/alunos', (req, res) => {
     res.status(200).send(alunos)
+})
+
+app.get('/alunos/:matricula', (req, res) => {
+    res.json(getAlunosByMatricula(req.params.matricula))
 })
 
 app.post('/alunos', (req, res) => {
     alunos.push(req.body)
     res.status(201).send('Aluno adicionado com sucesso!')
+})
+
+app.delete('alunos/:matricula', (req, res) => {
+    let index = buscarIndexAluno(matricula)
 })
 
 export default app
