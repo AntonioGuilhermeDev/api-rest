07ae8d517eb77b8dab7c7ca874f6c57ea7360341
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-04-27 11:35:22 -0300

    rota get para retornar alunos

--- src/app.js
+++ src/app.js
@@ -6,4 +6,14 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+const alunos = [
+    { matricula: 122221, nome: "Guilherme", curso: "Analise e Desenvolvimento de Sistemas" },
+    { matricula: 122222, nome: "Davy", curso: "Gestão de TI" },
+    { matricula: 122223, nome: "Daniel", curso: "Ciencia da Computação" }
+]
+
+app.get('/alunos', (req, res) => {
+    res.status(200).send(alunos)
+})
+
 export default app
